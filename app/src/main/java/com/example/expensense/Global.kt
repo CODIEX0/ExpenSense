e901f3cc17d39ed5898a614dfbabd62c0d30140c
Sibusiso <codyntuli@gmail.com>
2023-11-03 10:24:02 +0200

    fixed bugs

--- app/src/main/java/com/example/expensense/Global.kt
+++ app/src/main/java/com/example/expensense/Global.kt
@@ -1,11 +1,9 @@
 package com.example.expensense
 
 import com.example.expensense.data.Expense
-import java.util.Calendar
-import java.util.UUID
 
 //Singleton object for storing live data
 object Global {
-
+    var expenses: MutableList<Expense> = mutableListOf()
 
 }
